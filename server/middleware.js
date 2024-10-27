0185374cff31cf1e6792bf6c3a4e07c231001ea5
R3SONANC3 <jeerapat5870@gmail.com>
2024-10-28 00:00:18 +0700

    updaet cors

--- server/middleware.js
+++ server/middleware.js
@@ -10,11 +10,19 @@ const allowedOrigins = [
 ];
 
 const corsOptions = {
-    origin: '*',
+    origin: function (origin, callback) {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-  };
+    optionsSuccessStatus: 200, // This is important for older browsers
+};
+
   
 
 const verifyUser = (req, res, next) => {
